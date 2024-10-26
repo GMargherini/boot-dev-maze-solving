eb1f7059cad3afe13dc54090f81d9a11c092cdd8
Gmargherini <giorgio.margherini@gmail.com>
2024-10-26 17:08:35 +0100

    improved iterative wall breaking

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from maze import Maze
 from window import Window
 from point import Point
 
-rows = 20
+rows = 25
 cols = 30
 cell_size_x = 25
 cell_size_y = 25
 
 
 
-win = Window(cols*cell_size_y+20, rows*cell_size_x+20,)
+win = Window(cols*cell_size_y+20, rows*cell_size_x+20)
 
 m = Maze(rows, cols, cell_size_x, cell_size_y, win)
 m.solve()
--- maze.py
+++ maze.py
@@ -13,8 +13,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self.seed = seed
-        self._break_walls_r(0,0)
-        #self._break_walls_i()
+        self._break_walls_i(0,0)
         self._reset_cells_visited()
 
     def _create_cells(self):
@@ -40,57 +39,52 @@ class Maze:
         self._draw_cell(self.cells[self.cols-1][self.rows-1])
 
 
-    def _break_walls_i(self):
-        r = random.seed(self.seed)
+    def _break_walls_i(self,x,y):
+        random.seed(self.seed)
         to_visit = []
-        to_visit = [self.cells[i][j] for i in range(0,self.cols) for j in range(0,self.rows)]
-        random.shuffle(to_visit)
-        print(len(to_visit))
-        for here in to_visit:
-            print()
-            adjacient = []
+        to_visit.append(self.cells[x][y])
+        rest = []
+        
+        while len(to_visit):
+            here = to_visit.pop()
+            here.visited = True
+
             i = int(here.p1.x/(self.cell_size_x))
             j = int(here.p1.y/(self.cell_size_y))
 
-            if i != 0 and not self.cells[i-1][j].visited:
-                adjacient.append(self.cells[i-1][j])
-            if j != 0 and not self.cells[i][j-1].visited:
-                adjacient.append(self.cells[i][j-1])
-            if i+1 != self.cols and not self.cells[i+1][j].visited:
-                adjacient.append(self.cells[i+1][j])
-            if j+1 != self.rows and not self.cells[i][j+1].visited:
-                adjacient.append(self.cells[i][j+1])
+            adjacient = self.adjacient_cells(i,j)
             
-            print(here)
-            print(adjacient)
             if adjacient != []:
                 new_cell = random.choice(adjacient)
-                print(new_cell)
                 here.break_wall(new_cell)
-                here.visited = True
+                to_visit.extend(adjacient)
+                #adjacient = [i for i in adjacient if i != new_cell]
+                #random.shuffle(adjacient)
+                #rest.extend(adjacient)
+                #to_visit.append(new_cell)
+            #else:
+            #    rest = [i for i in rest if not i.visited]
+            #    if rest != []:
+            #        to_visit.append(rest[-1])
             self._draw_cell(here)
-
+            print(rest)
+            print(to_visit)
+            if [i for x in to_visit if not x.visited] == []:
+                break
 
     def _break_walls_r(self, i, j):
         here = self.cells[i][j]
         here.visited = True
         while True:
             to_visit = []
-            if i != 0 and not self.cells[i-1][j].visited:
-                to_visit.append((i-1,j))
-            if j != 0 and not self.cells[i][j-1].visited:
-                to_visit.append((i,j-1))
-            if i+1 != self.cols and not self.cells[i+1][j].visited:
-                to_visit.append((i+1,j))
-            if j+1 != self.rows and not self.cells[i][j+1].visited:
-                to_visit.append((i,j+1))
-
+            to_visit = self.adjacient_cells(i,j)
+            
             if to_visit == []:
                 self._draw_cell(self.cells[i][j])
                 return
             else:
                 r = random.seed(self.seed)
-                indexes = random.choice(to_visit)
+                indexes = random.choice(to_visit).coordinates
                 new_cell = self.cells[indexes[0]][indexes[1]]
                 here.break_wall(new_cell)
                 self._break_walls_r(indexes[0], indexes[1])
@@ -139,3 +133,24 @@ class Maze:
                 here.draw_move(self.cells[i][j+1], True)
         
         return False
+
+    def north(self,i,j):
+        return self.cells[i][j-1]
+    def east(self,i,j):
+        return self.cells[i+1][j]
+    def south(self,i,j):
+        return self.cells[i][j+1]
+    def west(self,i,j):
+        return self.cells[i-1][j]
+
+    def adjacient_cells(self, i, j):
+        adjacient = []
+        if i != 0 and not self.west(i,j).visited:
+            adjacient.append(self.west(i,j))
+        if j != 0 and not self.north(i,j).visited:
+            adjacient.append(self.north(i,j))
+        if i+1 != self.cols and not self.east(i,j).visited:
+            adjacient.append(self.east(i,j))
+        if j+1 != self.rows and not self.south(i,j).visited:
+            adjacient.append(self.south(i,j))
+        return adjacient
